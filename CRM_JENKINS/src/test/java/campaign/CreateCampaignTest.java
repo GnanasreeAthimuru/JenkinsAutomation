1b8768788190530763a068b63ea84b8432fcc16b
Gnanasree Athimuru <gnanasreeathimuru@gmail.com>
2024-10-18 20:12:53 +0530

    jenkinscode

---
+++ CRM_JENKINS/src/test/java/campaign/CreateCampaignTest.java
@@ -0,0 +1,13 @@
+package campaign;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCampaignTest {
+		@Test
+		public void createCampaign() {
+			Reporter.log("Created Campaign Successfully", true);
+		}
+
+	}
+
---
+++ CRM_JENKINS/src/test/java/organisation/CreateOrganisationTest.java
@@ -0,0 +1,12 @@
+package organisation;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateOrganisationTest {
+	@Test
+	public void createOrg() {
+		Reporter.log("Created Organization Successfully", true);
+	}
+
+}
